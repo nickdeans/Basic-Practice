c1611b14825ad063218823809042817fbb81a200
nickdeans <nickabramowicz@gmail.com>
2021-01-25 18:41:49 -0700

    basic practice css and js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,61 @@
 'use strict';
 
-var userName = prompt('What is your favorite color?');
+var userScore = 0;
+
+var pizza = prompt('Do you like pizza?');
 // alert(userName + ' is an awesome color!');
 
-///////////// Can be written like this code or the block below /////////////
+///////////// Can be written like this code or the block at bottom /////////////
 // if(userName === 'blue' || userName === 'Blue' || userName === 'BLUE'){
 //     console.log('nick likes blue');
 // } else{
 //     console.log('nick likes brown');
 // }
 
-if(userName.toLowerCase() === 'blue'){
-    console.log('nick likes blue');
+////Example of how to use console.log ///////////
+// if(userName.toLowerCase() === 'blue'){
+//     console.log('nick likes blue');
+// } else{
+//     console.log('nick likes brown');
+// }
+
+if(pizza.toLowerCase() === 'yes'){
+    alert('nick likes pizza');
+    userScore++;
+} else if(pizza.toLowerCase() === 'no'){
+    alert('nick does not like pizza');
 } else{
-    console.log('nick likes brown');
-}
+    alert('please answer with yes or no');
+};
 
-// var locationFrom = prompt('Where are you from?');
-// alert(locationFrom + ' is a great place to visit!');
+var locationFrom = prompt('Am I from Long Beach?');
 
-// var food = prompt('What is your favorite food?');
-// alert('I love ' + food + '!');
+if(locationFrom.toLowerCase() === 'yes'){
+    alert('You got it!');
+    userScore++;
+} else if(locationFrom.toLowerCase() === 'no'){
+    alert('you got it wrong');
+} else{
+    alert('please answer with yes or no');
+};
+
+var sushi = prompt('Do I like sushi?');
+
+if(sushi.toLowerCase() === 'yes'){
+    alert('Yes I like sushi!');
+    userScore++;
+} else if(sushi.toLowerCase() === 'no'){
+    alert('you got it wrong');
+} else{
+    alert('please answer with yes or no');
+};
+
+if(userScore === 0){
+    alert('you got em all wrong!');
+}else if(userScore === 1){
+    alert('you got 1 of 3');
+}else if(userScore === 2){
+    alert('you got 2 of 3');
+}else if(userScore === 3){
+    alert('you got all 3!');
+};
